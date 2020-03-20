5de2df276e82ce2ec12dd2ee6e05ba2a76b46cf4
Nick Muoh <nickspirit3+github@gmail.com>
2020-03-19 20:06:24 -0400

    Added UI components

---
+++ src/components/AppIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-icon"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class AppIcon extends Vue {
+  // @Prop() private appName!: string
+  // @Prop() private appUrl!: string
+}
+</script>
+
+<style scoped></style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="searchbar w-3/4">
+    <VInput
+      :classes="{
+        input:
+          'w-full px-3 py-2 appearance-none leading-tight \
+          border border-solid rounded-md \
+          focus:outline-none focus:border-primary'
+      }"
+      placeholder="Search..."
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { VInput } from 'vuetensils'
+
+@Component({
+  components: {
+    VInput
+  }
+})
+export default class SearchBar extends Vue {}
+</script>
+
+<style lang="scss"></style>
