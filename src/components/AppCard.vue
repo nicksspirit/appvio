37c90a6a09cf0532deadc26359d8d8f69b405906
Nick Muoh <nickspirit3+github@gmail.com>
2020-03-24 02:01:39 -0400

    Refactored app icon component to app card

---
+++ src/components/AppCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <dt>
+      {{ appData.name }}
+    </dt>
+    <dd>
+      <a href="">{{ appData.url }}</a>
+    </dd>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+import { AppData } from '@/common/interfaces'
+
+@Component
+export default class AppCard extends Vue {
+  @Prop() private appData!: AppData
+}
+</script>
+
+<style scoped></style>
