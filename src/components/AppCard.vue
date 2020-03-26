fc40379ecabc74ad15ba14be4b46656a5c9beb67
Nick Muoh <nickspirit3+github@gmail.com>
2020-03-25 21:43:31 -0400

    Added AppCard component

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,22 +1,108 @@
 <template>
-  <div>
-    <dt>
-      {{ appData.name }}
-    </dt>
-    <dd>
-      <a href="">{{ appData.url }}</a>
-    </dd>
+  <div
+    class="appcard max-w-sm rounded overflow-hidden shadow-md px-2 py-3 my-1"
+  >
+    <div class="appcard__icon">
+      <img v-show="faviconUrl" :src="faviconUrl" />
+      <svg
+        v-show="!faviconUrl"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        style="margin:auto;background:#fff;display:block;"
+        viewBox="0 0 100 100"
+        preserveAspectRatio="xMidYMid"
+      >
+        <circle
+          cx="50"
+          cy="50"
+          fill="none"
+          stroke="#292e66"
+          stroke-width="10"
+          r="35"
+          stroke-dasharray="164.93361431346415 56.97787143782138"
+        >
+          <animateTransform
+            attributeName="transform"
+            type="rotate"
+            repeatCount="indefinite"
+            dur="1s"
+            values="0 50 50;360 50 50"
+            keyTimes="0;1"
+          ></animateTransform>
+        </circle>
+      </svg>
+    </div>
+    <div class="appcard__details">
+      <dt>{{ appData.name }}</dt>
+      <dd>
+        <a href="">{{ appData.url }}</a>
+      </dd>
+    </div>
+    <div class="appcard__controls"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'vue-property-decorator'
 import { AppData } from '@/common/interfaces'
+import axios from 'axios'
+
+interface Icon {
+  sizes?: string
+  src: string
+  type?: string
+}
+
+interface FavIcon {
+  domain: string
+  icons: Icon[]
+}
 
 @Component
 export default class AppCard extends Vue {
+  public faviconUrl = ''
+  private readonly targetIconSize = '32'
   @Prop() private appData!: AppData
+
+  created() {
+    this.getFaviconUrl(this.$props.appData.url)
+  }
+
+  async getFaviconUrl(appUrl: string) {
+    const domain = new URL(appUrl).host
+    const fallbackUrl = `https://www.google.com/s2/favicons?domain=${domain}`
+    try {
+      const response = await axios.get<FavIcon>(
+        `http://favicongrabber.com/api/grab/${domain}`
+      )
+      const icons: Icon[] = response.data.icons
+
+      const bySizeOrType = (icon: Icon) => {
+        switch (icon.sizes ?? icon.type) {
+          case '32x32':
+          case 'image/png':
+          case 'image/x-icon':
+            return true
+          default:
+            return false
+        }
+      }
+
+      const iconUrls = icons.filter(bySizeOrType).map(icon => icon.src)
+
+      this.faviconUrl = iconUrls[0] !== undefined ? iconUrls[0] : fallbackUrl
+    } catch (error) {
+      this.faviconUrl = fallbackUrl
+    }
+  }
 }
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.appcard {
+  &__icon {
+    width: 24px;
+    height: 24px;
+  }
+}
+</style>
