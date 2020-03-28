e9960e143cd23c5a5a6a8746215ce30fdc031736
Nick Muoh <nickspirit3+github@gmail.com>
2020-03-28 01:22:32 -0400

    Added button controls (svg) to AppCard component

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,47 +1,102 @@
 <template>
   <div
     class="appcard max-w-sm rounded overflow-hidden \
-    shadow-md px-2 py-3 my-1 flex align-center"
+    shadow-md px-2 py-3 my-1 flex flex-col align-center"
   >
-    <div class="appcard__icon flex justify-center items-center mr-3 shadow-sm">
-      <div class="flex justify-center items-center">
-        <img v-show="faviconUrl" :src="faviconUrl" />
-        <svg
-          v-show="!faviconUrl"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          style="margin:auto;background:#fff;display:block;"
-          viewBox="0 0 100 100"
-          preserveAspectRatio="xMidYMid"
-        >
-          <circle
-            cx="50"
-            cy="50"
+    <div class="appcard__controls flex justify-end">
+      <div class="appcard__controls__btn menu-btn">
+        <button>
+          <svg
             fill="none"
-            stroke="#292e66"
-            stroke-width="10"
-            r="35"
-            stroke-dasharray="164.93361431346415 56.97787143782138"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
           >
-            <animateTransform
-              attributeName="transform"
-              type="rotate"
-              repeatCount="indefinite"
-              dur="1s"
-              values="0 50 50;360 50 50"
-              keyTimes="0;1"
-            ></animateTransform>
-          </circle>
-        </svg>
+            <path
+              d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+            ></path>
+          </svg>
+        </button>
       </div>
     </div>
-    <div class="appcard__details">
-      <dt class="">{{ appData.name }}</dt>
-      <dd class="">
+    <div class="flex flex-row">
+      <div
+        class="appcard__icon flex justify-center items-center mr-3 shadow-sm"
+      >
+        <div class="flex justify-center items-center">
+          <img v-show="faviconUrl" :src="faviconUrl" />
+          <svg
+            v-show="!faviconUrl"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            style="margin:auto;background:#fff;display:block;"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="xMidYMid"
+          >
+            <circle
+              cx="50"
+              cy="50"
+              fill="none"
+              stroke="#292e66"
+              stroke-width="10"
+              r="35"
+              stroke-dasharray="164.93361431346415 56.97787143782138"
+            >
+              <animateTransform
+                attributeName="transform"
+                type="rotate"
+                repeatCount="indefinite"
+                dur="1s"
+                values="0 50 50;360 50 50"
+                keyTimes="0;1"
+              ></animateTransform>
+            </circle>
+          </svg>
+        </div>
+      </div>
+      <div class="appcard__name">
+        <dt class="">
+          <h4 class="text-lg">{{ appData.name }}</h4>
+        </dt>
+        <!-- <dd class="">
         <a href="">{{ appData.url }}</a>
-      </dd>
+      </dd> -->
+      </div>
     </div>
-    <div class="appcard__controls"></div>
+    <div class="appcard__controls flex justify-end">
+      <div class="appcard__controls__btn copy-btn mx-1">
+        <button>
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <div class="appcard__controls__btn expand-btn mx-1">
+        <button>
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+          >
+            <path d="M19 9l-7 7-7-7"></path>
+          </svg>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -111,9 +166,17 @@ export default class AppCard extends Vue {
     width: 36px;
   }
 
-  &__icon div {
+  &__icon div,
+  &__icon svg {
     width: 24px;
     height: 24px;
+  }
+}
+
+.appcard__controls {
+  &__btn button {
+    width: 15px;
+    height: 15px;
   }
 }
 </style>
