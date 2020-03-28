0eaf618cf425249e4a64832dba3200050fd82ca6
Nick Muoh <nickspirit3+github@gmail.com>
2020-03-28 01:52:15 -0400

    Incremental improvements to AppCard components
    - Removed unnecessary wrapper divs
    - Truncate app name when it's too long

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -4,22 +4,20 @@
     shadow-md px-2 py-3 my-1 flex flex-col align-center"
   >
     <div class="appcard__controls flex justify-end">
-      <div class="appcard__controls__btn menu-btn">
-        <button>
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-            ></path>
-          </svg>
-        </button>
-      </div>
+      <button class="appcard__controls__btn menu-btn">
+        <svg
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+          ></path>
+        </svg>
+      </button>
     </div>
     <div class="flex flex-row">
       <div class="appcard__icon flex justify-center items-center mr-3">
@@ -56,7 +54,7 @@
       </div>
       <div class="appcard__name">
         <dt class="">
-          <h4 class="text-lg">{{ appData.name }}</h4>
+          <h4 class="text-lg truncate ...">{{ appData.name }}</h4>
         </dt>
         <!-- <dd class="">
         <a href="">{{ appData.url }}</a>
@@ -64,36 +62,32 @@
       </div>
     </div>
     <div class="appcard__controls flex justify-end">
-      <div class="appcard__controls__btn copy-btn mx-2">
-        <button>
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
-            ></path>
-          </svg>
-        </button>
-      </div>
-      <div class="appcard__controls__btn expand-btn mx-2">
-        <button>
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            viewBox="0 0 24 24"
-          >
-            <path d="M19 9l-7 7-7-7"></path>
-          </svg>
-        </button>
-      </div>
+      <button class="appcard__controls__btn copy-btn mx-2">
+        <svg
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
+          ></path>
+        </svg>
+      </button>
+      <button class="appcard__controls__btn expand-btn mx-2">
+        <svg
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          viewBox="0 0 24 24"
+        >
+          <path d="M19 9l-7 7-7-7"></path>
+        </svg>
+      </button>
     </div>
   </div>
 </template>
@@ -155,10 +149,14 @@ export default class AppCard extends Vue {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .appcard {
   width: inherit;
   height: inherit;
+
+  &__name {
+    max-width: 220px;
+  }
 
   &__icon {
     width: 36px;
@@ -171,7 +169,7 @@ export default class AppCard extends Vue {
   }
 
   &__controls {
-    &__btn button {
+    &__btn {
       width: 16px;
       height: 16px;
     }
