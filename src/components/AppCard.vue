b7e6bdb8e864c3f213144adc2a6a78f9b4c86234
Nick Muoh <nickspirit3+github@gmail.com>
2020-03-30 01:46:30 -0400

    Added app card description

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,91 +1,114 @@
 <template>
-  <div
-    class="appcard max-w-sm rounded overflow-hidden 
-    shadow-md px-2 py-3 my-1 flex flex-col align-center"
-  >
-    <div class="appcard__controls flex justify-end">
-      <button class="appcard__controls__btn menu-btn">
-        <svg
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
+  <div class="appcard relative">
+    <div
+      class="max-w-sm rounded overflow-hidden
+      shadow-md px-2 py-3 my-1 flex flex-col align-center relative z-20"
+    >
+      <div class="appcard__controls flex justify-end">
+        <button
+          class="appcard__controls__btn menu-btn focus:outline-none rounded-full focus:shadow-outline"
         >
-          <path
-            d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-          ></path>
-        </svg>
-      </button>
-    </div>
-    <div class="flex flex-row">
-      <div class="appcard__icon flex justify-center items-center mr-3">
-        <div class="flex justify-center items-center">
-          <img v-show="faviconUrl" :src="faviconUrl" />
           <svg
-            v-show="!faviconUrl"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
-            style="margin:auto;background:#fff;display:block;"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="xMidYMid"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
           >
-            <circle
-              cx="50"
-              cy="50"
-              fill="none"
-              stroke="#292e66"
-              stroke-width="10"
-              r="35"
-              stroke-dasharray="164.93361431346415 56.97787143782138"
-            >
-              <animateTransform
-                attributeName="transform"
-                type="rotate"
-                repeatCount="indefinite"
-                dur="1s"
-                values="0 50 50;360 50 50"
-                keyTimes="0;1"
-              ></animateTransform>
-            </circle>
+            <path
+              d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+            ></path>
           </svg>
-        </div>
+        </button>
       </div>
-      <dt class="appcard__name">
-        <a :href="appData.url">
-          <h4 class="text-lg truncate ...">{{ appData.name }}</h4>
-        </a>
-      </dt>
-    </div>
-    <div class="appcard__controls flex justify-end">
-      <button class="appcard__controls__btn copy-btn mx-2">
-        <svg
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
+      <div class="flex flex-row">
+        <div class="appcard__icon flex justify-center items-center mr-3">
+          <div class="flex justify-center items-center">
+            <img v-show="faviconUrl" :src="faviconUrl" />
+            <svg
+              v-show="!faviconUrl"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink"
+              style="margin:auto;background:#fff;display:block;"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="xMidYMid"
+            >
+              <circle
+                cx="50"
+                cy="50"
+                fill="none"
+                stroke="#292e66"
+                stroke-width="10"
+                r="35"
+                stroke-dasharray="164.93361431346415 56.97787143782138"
+              >
+                <animateTransform
+                  attributeName="transform"
+                  type="rotate"
+                  repeatCount="indefinite"
+                  dur="1s"
+                  values="0 50 50;360 50 50"
+                  keyTimes="0;1"
+                ></animateTransform>
+              </circle>
+            </svg>
+          </div>
+        </div>
+        <dt class="appcard__name">
+          <a :href="appData.url">
+            <h4 class="text-lg truncate ...">{{ appData.name }}</h4>
+          </a>
+        </dt>
+      </div>
+      <div class="appcard__controls flex justify-end">
+        <button
+          class="appcard__controls__btn copy-btn focus:outline-none rounded-full focus:shadow-outline mx-2"
         >
-          <path
-            d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
-          ></path>
-        </svg>
-      </button>
-      <button class="appcard__controls__btn expand-btn mx-2">
-        <svg
-          fill="none"
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          viewBox="0 0 24 24"
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
+            ></path>
+          </svg>
+        </button>
+        <button
+          :class="{
+            'appcard__controls__btn expand-btn mx-2 focus:outline-none rounded-full focus:shadow-outline': true,
+            'appcard__controls__btn--is-expanded': isExpanded
+          }"
+          @click="isExpanded = !isExpanded"
         >
-          <path d="M19 9l-7 7-7-7"></path>
-        </svg>
-      </button>
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            viewBox="0 0 24 24"
+          >
+            <path d="M19 9l-7 7-7-7"></path>
+          </svg>
+        </button>
+      </div>
     </div>
+    <transition name="slide-in-top">
+      <div
+        v-show="isExpanded"
+        class="appcard__details max-w-sm m-auto rounded overflow-hidden 
+        shadow-md px-2 py-3 my-1 flex flex-col align-center relative z-10"
+      >
+        <p class="px-2">
+          {{ appData.description }}
+        </p>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -108,6 +131,7 @@ interface FavIcon {
 @Component
 export default class AppCard extends Vue {
   public faviconUrl = ''
+  public isExpanded = false
   private readonly targetIconSize = '32'
   @Prop() private appData!: AppData
 
@@ -151,6 +175,10 @@ export default class AppCard extends Vue {
   width: inherit;
   height: inherit;
 
+  > div {
+    background-color: var(--color-bg);
+  }
+
   &__name {
     max-width: 220px;
   }
@@ -169,7 +197,66 @@ export default class AppCard extends Vue {
     &__btn {
       width: 16px;
       height: 16px;
+      transform: rotate(0deg);
+      transition: all ease-in-out 0.5s;
     }
+
+    &__btn--is-expanded {
+      transform: rotate(180deg);
+      transition: all ease-in-out 0.5s;
+    }
+  }
+
+  &__details {
+    height: 83px;
+    background-color: var(--color-bg);
+  }
+}
+
+.slide-in-top-enter-active {
+  -webkit-animation: slide-in-top 0.6s both;
+  animation: slide-in-top 0.6s both;
+}
+
+.slide-in-top-leave-active {
+  -webkit-animation: slide-in-top 0.6s reverse both;
+  animation: slide-in-top 0.6s reverse both;
+}
+
+/* ----------------------------------------------
+ * Generated by Animista on 2020-3-29 22:54:37
+ * Licensed under FreeBSD License.
+ * See http://animista.net/license for more info. 
+ * w: http://animista.net, t: @cssanimista
+ * ---------------------------------------------- */
+
+/**
+ * ----------------------------------------
+ * animation slide-in-top
+ * ----------------------------------------
+ */
+@-webkit-keyframes slide-in-top {
+  0% {
+    -webkit-transform: translateY(-83px);
+    transform: translateY(-83px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+@keyframes slide-in-top {
+  0% {
+    -webkit-transform: translateY(-83px);
+    transform: translateY(-83px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+    opacity: 1;
   }
 }
 </style>
