74f0677ed824d260865f33d65ba177be288dfae2
Nick Muoh <nickspirit3+github@gmail.com>
2020-04-11 13:33:38 -0400

    Improved UI of app card details card.
     - Description text dictates the height of detail card
    - tags don't overflow and scroll horizontally

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="appcard relative">
     <div
-      class="max-w-sm rounded overflow-hidden
-      shadow-md px-2 py-3 my-1 flex flex-col align-center relative z-20"
+      class="max-w-sm rounded overflow-hidden shadow-md px-2 py-3 my-1 flex flex-col align-center relative z-20"
     >
       <div class="appcard__controls flex justify-end">
         <button
@@ -18,7 +17,7 @@
           >
             <path
               d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-            ></path>
+            />
           </svg>
         </button>
       </div>
@@ -50,7 +49,7 @@
                   dur="1s"
                   values="0 50 50;360 50 50"
                   keyTimes="0;1"
-                ></animateTransform>
+                />
               </circle>
             </svg>
           </div>
@@ -75,12 +74,13 @@
           >
             <path
               d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"
-            ></path>
+            />
           </svg>
         </button>
         <button
           :class="{
-            'appcard__controls__btn expand-btn mx-2 focus:outline-none rounded-full focus:shadow-outline': true,
+            'appcard__controls__btn expand-btn mx-2 \
+            focus:outline-none rounded-full focus:shadow-outline': true,
             'appcard__controls__btn--is-expanded': isExpanded
           }"
           @click="isExpanded = !isExpanded"
@@ -93,7 +93,7 @@
             stroke-width="2"
             viewBox="0 0 24 24"
           >
-            <path d="M19 9l-7 7-7-7"></path>
+            <path d="M19 9l-7 7-7-7" />
           </svg>
         </button>
       </div>
@@ -101,12 +101,35 @@
     <transition name="slide-in-top">
       <div
         v-show="isExpanded"
-        class="appcard__details max-w-sm m-auto rounded overflow-hidden 
-        shadow-md px-2 py-3 my-1 flex flex-col align-center relative z-10"
+        class="
+        appcard__details max-w-sm m-auto h-auto
+        rounded overflow-hidden shadow-md 
+        px-1 py-1 my-1 flex flex-col 
+        relative z-10"
       >
-        <p class="px-2">
+        <p
+          class="appcard__details__descr px-2 mb-1 h-24 overflow-hidden text-justify"
+        >
           {{ appData.description }}
         </p>
+        <div
+          class="appcard__details__tags px-2 py-1 overflow-hidden overflow-x-scroll whitespace-no-wrap"
+        >
+          <span class="text-grey" v-if="appData.tags.length === 0">
+            No tags available
+          </span>
+          <span
+            :class="{
+              'bg-grey rounded-full text-white text-xxs p-1 mr-1':
+                appData.tags.length
+            }"
+            :key="index"
+            v-for="(tag, index) in appData.tags"
+            v-else
+          >
+            #{{ tag }}
+          </span>
+        </div>
       </div>
     </transition>
   </div>
@@ -171,6 +194,12 @@ export default class AppCard extends Vue {
 </script>
 
 <style lang="scss">
+.clamp {
+  display: -webkit-box;
+  -webkit-line-clamp: 3; // amount of line you want
+  -webkit-box-orient: vertical;
+}
+
 .appcard {
   width: inherit;
   height: inherit;
@@ -198,18 +227,32 @@ export default class AppCard extends Vue {
       width: 16px;
       height: 16px;
       transform: rotate(0deg);
-      transition: all ease-in-out 0.5s;
+      transition: transform ease-in-out 0.6s;
     }
 
     &__btn--is-expanded {
       transform: rotate(180deg);
-      transition: all ease-in-out 0.5s;
     }
   }
 
   &__details {
-    height: 83px;
     background-color: var(--color-bg);
+
+    &__tags::-webkit-scrollbar {
+      height: 5px;
+    }
+    &__tags {
+      scrollbar-width: thin;
+      scrollbar-color: var(--color-primary) var(--color-bg);
+    }
+    &__tags::-webkit-scrollbar-track {
+      background: var(--color-bg);
+    }
+    &__tags::-webkit-scrollbar-thumb {
+      background-color: var(--color-primary);
+      border-radius: 9999px;
+      border: 3px solid vvar(--color-bg);
+    }
   }
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: 'var(--color-primary)'
+        primary: 'var(--color-primary)',
+        grey: 'var(--color-grey)'
       },
       boxShadow: {
         outline: '0 0 0 3px rgba(41, 46, 102, 0.5)'
+      },
+      fontSize: {
+        xxs: '.65rem'
       }
     }
   },
